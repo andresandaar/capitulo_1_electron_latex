e135e30f2a7616ef55c9ba315aa2937d96e9c59a
andresandaar <davilaarroyo07@gmail.com>
2021-11-28 15:55:45 -0500

    primera version

---
+++ packagejson.m
@@ -0,0 +1,15 @@
+  {
+	"name": "electron_1",
+	"version": "1.0.0",
+	"description": "",
+	"main": "index.js",
+	"scripts": {
+		"start": "electron ."
+	},
+	"keywords": [],
+	"author": "",
+	"license": "ISC",
+	"devDependencies": {
+		"electron": "^16.0.0"
+	}
+   }
